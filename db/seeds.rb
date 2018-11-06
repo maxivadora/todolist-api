7c52693d90ba45eea28911b6492af6c001b54ce0
Maxi Vadora <maxivadora@gmail.com>
2018-11-06 11:59:35 -0300

    scaffold todo y problemas con CORS solucionado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create(title: "Primera tarea", completed: false)
+Todo.create(title: "Segunda tarea", completed: true)
+Todo.create(title: "Tercera tarea", completed: true)
+Todo.create(title: "Cuarta tarea", completed: false)
